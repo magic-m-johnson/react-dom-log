0b0fe655318bf9cdd8a716fa1a245682aa0ca6c6
Markus Michalsky <markus.michalsky@klarna.com>
2018-10-17 14:35:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React DOM log Enhancer
+
+`yarn add --dev react-dom-log`
+
+And load it at the beginning of your APP.
+
+Recommended for DEV only.
+
+Adds a (hopefully) unique prefix `[DOMLOG]` to full stack messages.
---
+++ index.js
@@ -0,0 +1,26 @@
+if (console && console.error) {
+    console._error = console.error
+    console.__error = (...args) => {
+        if (args.length > 1 && args[0].substr(-2) === '%s') {
+            let full = args.shift()
+            full = full.substr(0, full.length - 2).replace(/ *\n+ */, ' ')
+            const trace = args.pop()
+
+            while (full.indexOf('%s') >= 0 && args.length) {
+                full = full.replace(/%s/, args.shift())
+            }
+
+            let first = full.indexOf('.') >= 0 ? full.split('.')[0] + '.' : full
+            const type = first.indexOf('Warning') === 0 ? 'warn' : '_error'
+
+            console[type](first)
+            console._error('[DOMLOG] ' + full, {
+                trace: trace
+                    .trim()
+                    .split(/ *\n+ */)
+                    .map((a) => a.trim())
+            })
+        }
+    }
+}
+
